b98488319058c2e801cae18b127972ac89afe90a
Philipp Kapinos <blipkap@gmail.com>
2024-01-13 15:00:44 +0100

    added tts to solution, split for answers, textstyle changed

--- lib/widgets/math/CountingWidget.dart
+++ lib/widgets/math/CountingWidget.dart
@@ -2,6 +2,7 @@ import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../TextToSpeech.dart';
 import 'event/CountingEvent.dart';
 
 class CountingWidget extends StatelessWidget {
@@ -18,47 +19,71 @@ class CountingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final NumberBloc countingBloc = BlocProvider.of<NumberBloc>(context);
+    final TextToSpeech textToSpeech = TextToSpeech();
     return BlocProvider(
       create: (context) => countingBloc,
       child: Stack(alignment: Alignment.center, children: [
-         Scaffold(
-        appBar: AppBar(
-          title: Text('Number List'),
-        ),
-        body: BlocBuilder<NumberBloc, Map<String, List<int>>>(
-          builder: (context, numbers) {
-            var list = numbers["Q"];
-            return Column(
-              children: [
-                SizedBox(
-                  height: 400,
-                  child: ListView.builder(
-                    itemCount: list!.length,
-                    itemBuilder: (context, index) {
-                      return NumberTile(number: list[index]);
-                    },
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Number List'),
+          ),
+          body: BlocBuilder<NumberBloc, Map<String, List<int>>>(
+            builder: (context, numbers) {
+              var list = numbers["Q"];
+              return Column(
+                children: [
+                  SizedBox(
+                    height: 400,
+                    child: ListView.builder(
+                      itemCount: list!.length,
+                      itemBuilder: (context, index) {
+                        return NumberTile(number: list[index]);
+                      },
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ...numbers["A"]!
-                        .map((value) => ElevatedButton(
-                            onPressed: () {
-                              if(value == numbers["S"]!.first);
-
-                              countingBloc.add(NumberPressedEvent());
-                            }, child: Text(value.toString())))
-                        .toList(),
-                  ],
-                ),
-              ],
-            );
-          },
+                  SizedBox(height: 20),
+                  buildButtonRow(
+                      numbers["A"]!.sublist(0, numbers["A"]!.length ~/ 2),
+                      numbers,
+                      textToSpeech,
+                      countingBloc),
+                  SizedBox(height: 20),
+                  buildButtonRow(
+                      numbers["A"]!.sublist(numbers["A"]!.length ~/ 2),
+                      numbers,
+                      textToSpeech,
+                      countingBloc),
+                ],
+              );
+            },
+          ),
         ),
-      ),
-    ]),
+      ]),
+    );
+  }
+
+  Row buildButtonRow(List<int> buttons, Map<String, List<int>> numbers,
+      TextToSpeech textToSpeech, NumberBloc countingBloc) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: buttons
+          .map((value) => ElevatedButton(
+              style: ElevatedButton.styleFrom(padding: EdgeInsets.all(50.0)),
+              onPressed: () {
+                if (value == numbers["S"]!.first) {
+                  textToSpeech.speak("Das ist richtig!");
+                } else {
+                  textToSpeech.speak("Das war leider nichts!");
+                }
+                Future.delayed(Duration(seconds: 1), () {
+                  countingBloc.add(NumberPressedEvent());
+                });
+              },
+              child: Text(
+                value.toString(),
+                style: TextStyle(fontSize: 20.0),
+              )))
+          .toList(),
     );
   }
 }
